a1f7b4f8fc274d27c53da84e49381371caf223fd
Mani Mishra <manimishra902@gmail.com>
2019-04-07 23:15:44 +0530

    remove old project and update firebase config

---
+++ m2-table/src/firebase/index.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+
+// Initialize Firebase
+// copy this config from your firebase console
+const config = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
+};
+
+firebase.initializeApp(config);
