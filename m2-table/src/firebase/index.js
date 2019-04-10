0f6a144eaa853b6d654bbd3bb154a65e2c9648d7
Mani Mishra <manimishra902@gmail.com>
2019-04-10 19:56:21 +0530

    add firebase config back (even though API keys should never be checkedin) to make local cloning and running easier

--- m2-table/src/firebase/index.js
+++ m2-table/src/firebase/index.js
@@ -1,14 +1,15 @@
 import firebase from "firebase";
 
 // Initialize Firebase
-// copy this config from your firebase console
+
+// config for demo site, replace with it your own config and data in expected structure if needed, see readme
 const config = {
-  apiKey: "",
-  authDomain: "",
-  databaseURL: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: ""
+  apiKey: "AIzaSyC-aPQ7aEhdbE29jOa9jelRISrsdSByfHU",
+  authDomain: "data-table-vue.firebaseapp.com",
+  databaseURL: "https://data-table-vue.firebaseio.com",
+  projectId: "data-table-vue",
+  storageBucket: "data-table-vue.appspot.com",
+  messagingSenderId: "1079528810421"
 };
 
 firebase.initializeApp(config);
